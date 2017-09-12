f0e301652784a837f199ab97f069e35b71385fdc
fake2edison <a632465734@126.com>
2017-09-12 09:47:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fake2edison/test1/SecondActivity.java
@@ -0,0 +1,10 @@
+package com.example.fake2edison.test1;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by fake2edison on 2017/9/12.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+}
